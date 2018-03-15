87966beec4c8d9abc01d2159481640ab5ac53f7a
Kuvar Singh <kuvarjava@gmail.com>
2018-03-15 23:30:00 +0530

    MVVMLearning
    MVVMLearning

---
+++ MVVMLearning/MVVMLearning/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Timers;
+
+namespace MVVMLearning.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        System.Timers.Timer timer;
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; RaisPropertyChanged(() => Name); }
+        }
+
+        private string _time;
+
+        public string Time
+        {
+            get { return _time; }
+            set { _time = value;  RaisPropertyChanged(() => Time); }
+        }
+
+
+        public MainPageViewModel()
+        {
+            _name = "Pankaj Singh";
+
+            timer = new System.Timers.Timer(); timer.Interval = 1000; timer.Elapsed += elapsed; timer.Enabled = true;
+
+        }
+
+        private void elapsed(object sender, ElapsedEventArgs e)
+        {
+            DateTime t = e.SignalTime;
+            _time = t.ToLongTimeString();
+            RaisPropertyChanged(() => Time);
+        }
+    }
+}
---
+++ MVVMLearning/MVVMLearning/ViewModels/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MVVMLearning.ViewModels
+{
+    public class ViewModelBase : ExtendedBindableObject
+    {
+        public INavigation Navigation { get; set; }
+
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set
+            {
+                _isBusy = value;
+                RaisPropertyChanged(() => IsBusy);
+            }
+        }
+
+    }
+}
